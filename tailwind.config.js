6d1fd52e815d0d76dcb2af6a06a2055f8be6e6a0
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-02-25 23:07:38 +0200

    fix: laptop no longer breaks on half screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        h1clamp: "clamp(2.8rem, 5.5vw, 6rem)",
-        h2clamp: "clamp(2.35rem, 4.5vw, 5rem)",
+        h1clamp: "clamp(2.5rem, 5.2vw, 6rem)",
+        h2clamp: "clamp(2rem, 4.5vw, 5rem)",
         h3clamp: "clamp(1.5rem, 3vw, 3rem)",
         labelClamp:"clamp(1.3rem, 1.8vw, 2.3rem)",
         iconClamp: "clamp(4em, 10vw, 13rem)",
