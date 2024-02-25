9f289d86a1b3b49f3ad66fe019f7b24a57d33bc9
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-02-25 11:01:55 +0200

    feat: error validation for contact form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,8 +5,9 @@ import { buttonStyle } from '../components/Hero'
 import { links } from '../constants/links'
 import { useEffect, useRef, useState } from 'react';
 
-const inputStyles = "focus:outline-none w-[100%] lg:w-[95%] mb-5 px-2 py-3 text-2xl rounded-md text-white bg-background border-2 border-purple"
-const labelStyles = "font-mono text-lightGray text-labelClamp"
+const inputStyles = "focus:outline-none w-[100%] lg:w-[95%] px-2 py-3 mb-5 text-2xl rounded-md text-white bg-background border-2 border-purple"
+const labelStyles = "font-mono text-lightGray text-2xl"
+const labelErrorStyles = "font-mono text-red-600 text-2xl"
 export const ContactMe = () => {
     const {
         register,
@@ -55,21 +56,30 @@ export const ContactMe = () => {
                 <AccentTitle text={"Like what you're seeing?"} classes="mr-auto" />
             </animated.section>
             <div className="max-w-[1800px  min-h-[90vh] lg:min-h-[45vh] py-4 relative w-[90%] justify-between flex flex-col lg:flex-row items-start">
-                <animated.form onSubmit={handleSubmit(onSubmit)} style={slideAnimation} className="flex-2  w-full flex flex-col lg:max-w-[42%] justify-evenly items-start rounded-lg">
+                <animated.form onSubmit={handleSubmit(onSubmit)} style={slideAnimation} className="flex-2  w-full flex p-4 flex-col border-2 border-white lg:max-w-[42%] justify-evenly items-start rounded-xl">
                     <SecondaryTitle text={'Get in touch now'} classes='mb-5'/>
-                    <label className={labelStyles}>{errors.message ? "Please enter a message" :"What's your name?"}</label>
-                    <input {...register("name")} type="text" className={inputStyles} />
-                    <label className={labelStyles}>{errors.message ? "Please enter a message" :"Your favourite email?"}</label>
-                    <input type="text" {...register("email")} className={inputStyles} />
-                
-                    <label className={labelStyles}>{errors.message ? "Please enter a message" :"What's on your mind?"}</label>
-                    <input type="text" {...register("message")} className={inputStyles} />
-                    <button type="submit" className={`${buttonStyle} mb-2`}>Let's have a chat!</button>
+                    {errors.name ?
+                    <label className={labelErrorStyles}> {errors.name.message}</label> 
+                    :<label className={labelStyles}>What's your name?</label>
+                    }
+                    <input {...register("name", {required:'Please enter a name', minLength:3})} type="text" className={inputStyles} />
+                    {errors.email ?
+                    <label className={labelErrorStyles}> {errors.email.message}</label> 
+                    :<label className={labelStyles}>"Your favourite email?"</label>
+                    }
+                    <input type="text" {...register("email",{required:'Please enter a valid email',pattern:
+                            /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,})} className={inputStyles} />
+                {errors.message ?
+                    <label className={labelErrorStyles}> {errors.message.message}</label> 
+                    :<label className={labelStyles}>What's on your mind?</label>
+                    }
+                    <input type="text" {...register("message", {required:'Please leave a message'})} className={inputStyles} />
+                    <button type="submit" className={`${buttonStyle} my-2`}>Let's have a chat!</button>
                 </animated.form>
                 <animated.div style={slideAnimation} className="text-center flex-1 py-8 lg:py-3 self-center"><AccentTitle text="Or..." /></animated.div>
-                <animated.section style={slideAnimation} className="text-white flex-2 w-full lg:max-w-[42%] flex flex-col justify-between items-start">
+                <animated.section style={slideAnimation} className="text-white p-4  border-white border-2 flex-2 w-full lg:max-w-[42%] rounded-xl flex flex-col justify-between items-start">
                     <h1 className="text-h2clamp whitespace-nowrap mb-5 font-semibold"> Find me on...</h1>
-                    <div className=" text-3xl flex flex-col rounded-xl flex-wrap items-start justify-evenly h-full  border-2 text-lightGray border-white w-full p-4 my-2 md:my-0">
+                    <div className=" text-3xl flex flex-col rounded-xl flex-wrap items-start justify-evenly h-full text-lightGray  w-full my-2 md:my-0">
                        {links.map(link => <LinkPair key={link.text} text={link.text} href={link.href} button={link.button} icon={link.icon} />)}
                     </div>
                 </animated.section>
@@ -83,7 +93,7 @@ export const ContactMe = () => {
 const LinkPair = ({href, text, icon}) => {
     return (
         <div
-        className="flex bg-gray bg-opacity-10 gap-2 rounded-lg text-2xl p-1 justify-start w-full items-center mb-5">
+        className="flex bg-gray bg-opacity-10 gap-2 rounded-lg text-2xl p-[11px] justify-start w-full items-center mb-5">
             {icon}
             <p>{text}</p>
             <a href={href} className={`${buttonStyle} ml-auto text-sm px-6 my-auto`}> Go</a>
