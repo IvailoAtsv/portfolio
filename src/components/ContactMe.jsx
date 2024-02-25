7f923b658f69d0426785192d251a905d83c7cf8c
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-02-25 19:09:52 +0200

    feat: loader and thanks you message after send

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,14 +5,16 @@ import { AccentTitle, SecondaryTitle } from "./ReusableComponents/Titles"
 import { buttonStyle } from '../components/Hero'
 import { links } from '../constants/links'
 import emailjs from '@emailjs/browser'
+import { FaHeart } from "react-icons/fa";
+import Loader from 'react-loaders'
 
 const inputStyles = "focus:outline-none w-[100%] lg:w-[95%] px-2 py-3 mb-5 text-2xl rounded-md text-white bg-background border-2 border-purple"
 const labelStyles = "font-mono text-lightGray text-2xl"
 const labelErrorStyles = "font-mono text-red-600 text-2xl"
 export const ContactMe = () => {
 
-  const [disabled,setDisabled] = useState(false)
-
+  const [sent,setSent] = useState(false)
+  const [loading,setLoading] = useState(false)
     const {
         register,
         handleSubmit,
@@ -20,12 +22,15 @@ export const ContactMe = () => {
         formState: { errors },
       } = useForm()
       const onSubmit =async (data) => {
+        setLoading(true)
         const params = {
           name:data.name,
           email:data.email,
           message:data.message
         }
         await emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE,params, process.env.REACT_APP_PUBLIC)
+        setLoading(false)
+        setSent(true)
       }
     // dear god, these animations are not worth the effort
     const componentRef = useRef(null);
@@ -37,7 +42,6 @@ export const ContactMe = () => {
         config: { tension: 220, friction: 30 },
         delay:300
       });
-
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
           entries.forEach((entry) => {
@@ -66,7 +70,11 @@ export const ContactMe = () => {
                 <AccentTitle text={"Like what you're seeing?"} classes="mr-auto" />
             </animated.section>
             <div className="max-w-[1800px] min-h-[90vh] lg:min-h-[45vh] py-4 relative w-[90%] justify-between flex flex-col lg:flex-row items-start">
-                <animated.form onSubmit={handleSubmit(onSubmit)} style={slideAnimation} className="flex-2  w-full flex p-4 flex-col border-white lg:max-w-[42%] justify-evenly items-start rounded-xl">
+                {sent 
+                ? (<div className="flex-2 w-full flex p-4 flex-col lg:max-w-[42%] justify-center items-start rounded-xl">
+                  {loading? <h2 className='text-4xl text-white font-semibold animate-pulse'>Loading...</h2> :<h2 className='text-4xl text-white font-semibold'>Thanks for contacting me <FaHeart className='inline'/><br/>I'll do my best to reply soon </h2>}
+                </div>)
+                :<animated.form onSubmit={handleSubmit(onSubmit)} style={slideAnimation} className="flex-2  w-full flex p-4 flex-col border-white lg:max-w-[42%] justify-evenly items-start rounded-xl">
                     <SecondaryTitle text={'Get in touch now'} classes='mb-5'/>
                     {errors.name ?
                     <label className={labelErrorStyles}> {errors.name.message}</label> 
@@ -84,8 +92,8 @@ export const ContactMe = () => {
                     :<label className={labelStyles}>What's on your mind?</label>
                     }
                     <input type="text" {...register("message", {required:'Please leave a message'})} className={inputStyles} />
-                    <button type="submit" disabled={disabled} className={`${buttonStyle} my-2`}>Let's have a chat!</button>
-                </animated.form>
+                    <button type="submit" disabled={sent} className={`${buttonStyle} my-2`}>Let's have a chat!</button>
+                </animated.form>}
                 <animated.div style={slideAnimation} className="text-center w-full flex-1 py-8 lg:py-3 self-center">
                     <AccentTitle text="Or..." classes="overflow-hidden z-50"/>
                     </animated.div>
