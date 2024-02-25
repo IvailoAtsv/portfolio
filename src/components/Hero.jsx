bb6a6d28650eeb9b0c34d2f52a0733efdec67712
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-02-25 19:39:38 +0200

    fix: style fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export const Hero = () => {
 }, [])
     return (
         <div id="home" className="w-full py-10 relative h-screen md:my-auto overflow-x-hidden bg-background flex justify-center items-center">
-                {showArrow && <FaArrowDown className={`animate-bounce text-lightGray absolute lg:bottom-[3%] bottom-[8%] mx-auto`} size={40} />}
+                {showArrow && <FaArrowDown className={`animate-bounce text-lightGray absolute lg:bottom-[3%] bottom-[5%] mx-auto`} size={40} />}
             <div className="max-w-[1800px] pb-10 w-[90%] h-[100%] justify-center lg:justify-between flex sm:gap-2 gap-12 items-center flex-col-reverse lg:flex-row ">
                 <animated.div style={enterLeft} className="w-[90%] lg:w-[50%] max-w-[1000px] flex flex-col justify-center gap-2 items-center md:items-start">
                     <div className="lg:w-[50%] h-[100%] w-[100%]">
