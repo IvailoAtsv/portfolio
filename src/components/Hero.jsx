76f7c68422d1aa03ad77a58ed4732ddb72f9e9a7
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-02-25 21:48:13 +0200

    fix: arrow down placement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,44 @@
 import { useEffect, useState } from "react";
 import uniqid from 'uniqid'
-import {FaArrowDown} from "react-icons/fa"
+import { FaArrowDown } from "react-icons/fa"
 import Marquee from 'react-fast-marquee';
 import { useSpring, animated } from 'react-spring';
-import {enterRightAnimation, enterLeftAnimation} from '../constants/animations'
+import { enterRightAnimation, enterLeftAnimation } from '../constants/animations'
 import { MainTitle, SecondaryTitle, AccentTitle } from "./ReusableComponents/Titles";
 import laptop from '../images/laptop.png'
 import { icons } from "../constants/skillIcons";
-import {saveAs} from 'file-saver'
+import { saveAs } from 'file-saver'
 import CV from '../other/CV.pdf'
 
 export const buttonStyle = "bg-background self-start p-2 rounded-lg transition duration-300 text-md border-purple border-[2px] text-purple hover:bg-purple hover:border-background hover:text-black font-mono"
 
 
 export const Hero = () => {
-    const enterLeft = useSpring({...enterLeftAnimation});
-    const enterRight = useSpring({...enterRightAnimation});
+    const enterLeft = useSpring({ ...enterLeftAnimation });
+    const enterRight = useSpring({ ...enterRightAnimation });
 
     const [showArrow, setShowArrow] = useState(false)
 
-    const downloadCV = () =>{
-        saveAs(CV,"Ivaylo's CV")
+    const downloadCV = () => {
+        saveAs(CV, "Ivaylo's CV")
     }
 
-    useEffect(()=>{
-    setTimeout(() => {
-        setShowArrow(true)
+    useEffect(() => {
         setTimeout(() => {
-            setShowArrow(false)
-        }, 10000);
-    }, 2000);
-}, [])
+            setShowArrow(true)
+            setTimeout(() => {
+                setShowArrow(false)
+            }, 10000);
+        }, 2000);
+    }, [])
     return (
-        <div id="home" className="w-full py-10 relative h-screen md:my-auto overflow-x-hidden bg-background flex justify-center items-center">
-                {showArrow && <FaArrowDown className={`animate-bounce text-lightGray absolute lg:bottom-[3%] bottom-[5%] mx-auto`} size={40} />}
+        <div id="home" className="w-full py-10 relative h-[95vh] md:my-auto overflow-x-hidden bg-background flex justify-center items-center">
+            {showArrow && <FaArrowDown className={`animate-bounce text-lightGray absolute lg:bottom-[3%] bottom-[5%] mx-auto`} size={40} />}
             <div className="max-w-[1800px] pb-10 w-[90%] h-[100%] justify-center lg:justify-between flex sm:gap-2 gap-12 items-center flex-col-reverse lg:flex-row ">
                 <animated.div style={enterLeft} className="w-[90%] lg:w-[50%] max-w-[1000px] flex flex-col justify-center gap-2 items-center md:items-start">
                     <div className="lg:w-[50%] h-[100%] w-[100%]">
-                        <AccentTitle text={'Hi, my name is...'}/>
-                        <MainTitle classes="whitespace-nowrap" text ={'Ivaylo Atanasov.'}/>
+                        <AccentTitle text={'Hi, my name is...'} />
+                        <MainTitle classes="whitespace-nowrap" text={'Ivaylo Atanasov.'} />
                         <SecondaryTitle classes="whitespace-nowrap" text={"I am a web developer"} />
                         <h4 className="text-lightGray py-2">Passionate about front-end and full stack development, I possess a keen eye for design and a genuine love for crafting seamless and intuitive user experiences. My expertise lies in transforming concepts into visually striking and responsive web applications. </h4>
 
@@ -48,13 +48,13 @@ export const Hero = () => {
                 </animated.div>
 
                 <animated.div style={enterRight} className="lg:w-[50%] w-[90%] relative max-w-[750px] overflow-hidden">
-    <img alt="Animation displaying all of my skills" src={laptop} className="w-[100%]" />
-    <div className='shadow-md absolute top-[2.3%] bg-black left-[13.5%] max-w-[600px] w-[73%] h-[65.5%] text-lightGray my-auto'>
-        <Marquee direction="left" pauseOnHover={true} play={true} className="my-auto h-full" speed={80}>
-            {icons.map((icon) => <SkillIcon key={uniqid()} icon={icon.icon} title={icon.title} />)}
-        </Marquee>
-    </div>
-</animated.div>
+                    <img alt="Animation displaying all of my skills" src={laptop} className="w-[100%]" />
+                    <div className='shadow-md absolute top-[2.3%] bg-black left-[13.5%] max-w-[600px] w-[73%] h-[65.5%] text-lightGray my-auto'>
+                        <Marquee direction="left" pauseOnHover={true} play={true} className="my-auto h-full" speed={80}>
+                            {icons.map((icon) => <SkillIcon key={uniqid()} icon={icon.icon} title={icon.title} />)}
+                        </Marquee>
+                    </div>
+                </animated.div>
 
 
             </div>
