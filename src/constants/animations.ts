79717bd4557727332e874372c23f35e2d066fdf4
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-03-11 16:22:01 +0200

    fix: added guiding text and made animations smoother

--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -1,13 +1,13 @@
 
 export const enterRightAnimation ={
-    from: { opacity: 0, transform: 'translateX(200%)' },
-    to: { opacity: 1, transform: 'translateX(0%)' },
+    from: {  transform: 'translateX(200%)' },
+    to: {transform: 'translateX(0%)' },
     config: { duraton:400 ,tension: 220, friction: 30 },
      delay: 350 ,
   }
 export const enterLeftAnimation ={
-    from: { opacity: 0, transform: 'translateX(-200%)' },
-    to: { opacity: 1, transform: 'translateX(0%)' },
+    from: {  transform: 'translateX(-200%)' },
+    to: {  transform: 'translateX(0%)' },
     config: { duraton:400 ,tension: 220, friction: 30 },
      delay: 350 ,
   }
